81d4ba27a623b48234af04bd8c940ecada2be55c
Sneha Das <154408198+aishseojudin@users.noreply.github.com>
2024-02-19 23:03:01 +0530

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -10,22 +10,20 @@ def number_guessing():
 
     print("\n\tYou've only ", round(math.log(high - lower + 1, 2)),"chances to guess the integer!\n")
     count = 0
-    guess = int(input("Enter you guessed number"))
 
-    while True:
-        count+=1
-        if n < guess:
+    while count < math.log(high - lower + 1, 2):
+        guess = int(input("Enter you guessed number"))
+        if n == guess:
+            print(f"Hurraay!! You made the right guess after {count} trials!")
+            break
+        elif n < guess:
             print("Try Again! You guessed higher")
         elif n > guess:
             print("Try Again! You guessed lower")
-        else:
-            print(f"Hurraay!! You made the right guess after {count} trials!")
-            break
-        
-        if count >= math.log(high - lower + 1, 2):
-            print(f"Oops!! The number is {n}. Better luck next time! ")
-            break
+        count += 1
 
+    if n != guess:
+        print(f"Oops!! The number is {n}. Better luck next time!")
     play_again = input("Do you want to play again? (yes/no): ")
     if play_again.lower() == "yes":
         number_guessing()
@@ -40,5 +38,25 @@ def number_guessing():
         print("Please enter valid integers for the range")
         number_guessing()
 
+
+def computer_guess():
+    print("Now let the computer guess your magic number")
+    low = 1
+    high = int(input("Enter a number as upper limit"))
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+
 if __name__ == "__main__":
     number_guessing()
+    computer_guess()
